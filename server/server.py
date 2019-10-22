dfe7b0d79e70683163b2b0349fbc2bce7a418c10
khanhtran <tran_khanh@outlook.com>
2019-10-22 23:04:23 +0700

    update server url

--- server/server.py
+++ server/server.py
@@ -6,9 +6,11 @@ from flask_cors import CORS
 app = Flask(__name__,static_folder='./')
 CORS(app)
 
+HOST='192.168.1.125'
+
 @app.route("/")
 def hello():
-    return "Backend for Dressing-Room!"
+    return "<h1>Backend for Dressing room</h1><a href='/get_init_style'>GET ALL MODEL PART: /get_init_style</a></br><a href='/get_shop_data'>GET ALL SHOPS ITEM PART: /get_shop_data</a>"
 
 @app.route("/get_init_style")
 def getInitStyle():
