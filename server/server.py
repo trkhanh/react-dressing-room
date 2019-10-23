aa7351b4cf5293b347f1fbbd995c41a84f5b4b68
KhanhTran <khanhtran@gooappable.com>
2019-10-23 17:56:55 +0700

    BE: update SERVER

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ HOST='192.168.1.125'
 
 @app.route("/")
 def hello():
-    return "<h1>Backend for Dressing room</h1><a href='/get_init_style'>GET ALL MODEL PART: /get_init_style</a></br><a href='/get_shop_data'>GET ALL SHOPS ITEM PART: /get_shop_data</a>"
+    return "<h1>Backend for Dressing room</h1><a href='/get_init_style'>GET ALL MODEL PART: /get_init_style</a></br><a href='/get_shop_data'>GET ALL SHOPS ITEM PART: /get_shop_data</a></br><a href='/get_button_array'>GET ALL BUTTONS: /get_button_array</a>"
 
 @app.route("/get_init_style")
 def getInitStyle():
@@ -22,6 +22,13 @@ def getInitStyle():
 @app.route("/get_shop_data")
 def getShopData():
     filename = os.path.join(app.static_folder, 'shopData.json')
+    with open(filename) as styles:
+        data = json.load(styles)
+    return data
+
+@app.route("/get_button_array")
+def getShopData():
+    filename = os.path.join(app.static_folder, 'buttonArray.json')
     with open(filename) as styles:
         data = json.load(styles)
     return data
